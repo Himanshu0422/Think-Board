5d2db884ed358c8b5f51c83366ce57190f8a1842
Himanshu0422 <himanshumittal035@gmail.com>
2024-09-25 18:36:42 +0530

    first commit shadcn convex clerk setup

---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://prompt-airedale-48.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ]
+};
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['img.clerk.com'], // Allow images from img.clerk.com
+  },
+};
 
 export default nextConfig;
